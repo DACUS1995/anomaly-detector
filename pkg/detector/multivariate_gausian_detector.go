a89ad0a31e8bd5af826d508fc18601e1d1e8838e
DACUS1995 <studormarian@gmail.com>
2020-07-03 12:47:10 +0300

    Params loading + tests

--- pkg/detector/multivariate_gausian_detector.go
+++ pkg/detector/multivariate_gausian_detector.go
@@ -1,8 +1,12 @@
 package detector
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/DACUS1995/datacenter-anomaly-detector/pkg/dataset"
 )
@@ -10,53 +14,91 @@ import (
 type MultivariateGaussianDetector struct {
 	parametersPath *string
 	datasetPath    *string
+	parameters     *Parameters
+}
+
+type Parameters struct {
+	MeanArr   []float32   `json:"MeanArr"`
+	CovMatrix [][]float32 `json:"CovMatrix"`
 }
 
 func NewMultivariateGaussianDetector(parametersPath *string, datasetPath *string) (*MultivariateGaussianDetector, error) {
+	if datasetPath == nil && parametersPath == nil {
+		return nil, errors.New("Only one parameters can have a nil value")
+	}
+
 	if parametersPath == nil {
 		return &MultivariateGaussianDetector{
 			datasetPath: datasetPath,
+			parameters:  &Parameters{},
 		}, nil
 	} else {
-		if datasetPath == nil {
-			return nil, errors.New("Only one paramters can have a nil value.")
-		}
-
 		return &MultivariateGaussianDetector{
 			parametersPath: parametersPath,
+			parameters:     &Parameters{},
 		}, nil
 	}
 }
 
-func (detector *MultivariateGaussianDetector) Init() {
+func (detector *MultivariateGaussianDetector) Init() error {
+	if detector.parametersPath == nil && detector.datasetPath == nil {
+		return errors.New("No parameters path or dataset path was set")
+	}
+
 	if detector.parametersPath == nil {
 		dataset := dataset.NewCSVDataset(*detector.datasetPath)
-		detector.computeParamters(dataset)
+		detector.computeParameters(dataset)
 	} else {
 		detector.loadParameters()
 	}
+
+	return nil
 }
 
 func (detector *MultivariateGaussianDetector) Detect(x *dataset.SimpleDatapoint) {
 
 }
 
-func (detector *MultivariateGaussianDetector) computeParamters(dataset dataset.Dataset) {
+func (detector *MultivariateGaussianDetector) Save() {
+
+}
+
+func (detector *MultivariateGaussianDetector) computeParameters(dataset dataset.Dataset) {
 	sampleSize := len(dataset.GetSamples()[0].Values)
 
+	// Init parameters
 	meanArr := make([]float32, sampleSize)
 	covMatrix := make([][]float32, sampleSize)
+
 	for idx := range covMatrix {
 		covMatrix[idx] = make([]float32, sampleSize)
 	}
 
+	detector.parameters = &Parameters{
+		MeanArr:   meanArr,
+		CovMatrix: covMatrix,
+	}
+
+	// Compute parameters
 	for _, elem := range dataset.GetSamples() {
 		for idx, value := range elem.Values {
 			meanArr[idx] += value
 		}
 	}
+
+	log.Println("Parameters computed loaded.")
 }
 
 func (detector *MultivariateGaussianDetector) loadParameters() {
+	jsonFile, err := os.Open(*detector.parametersPath)
+	if err != nil {
+		fmt.Printf("Precomputed parameters file not found %v", *detector.parametersPath)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &detector.parameters)
+
 	log.Println("Detector loaded.")
 }
